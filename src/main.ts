0156e8693fa1234a26d0f42dcaed4f5fe980ff29
Aleksi Kytölä <aleksi.kytola@reaktor.com>
2023-02-23 11:54:30 +0200

    Dampening & Sampling curve

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,41 @@ const GRAY: Color = { r: 122, g: 122, b: 122 };
 const PURPLE: Color = { r: 148, g: 0, b: 211 };
 const BLACK: Color = { r: 0, g: 0, b: 0 };
 
+interface SamplingCurvePoint {
+	x: number;
+	y: number;
+}
+
+class SamplingCurve {
+	points_: SamplingCurvePoint[];
+	constructor(points: SamplingCurvePoint[]) {
+		this.points_ = points;
+		this.points_.sort((a, b) => a.x - b.x);
+		console.log(this.points_);
+	}
+
+	getValue(x: number) {
+		let previousPoint: SamplingCurvePoint | undefined;
+		let nextPoint: SamplingCurvePoint | undefined;
+
+		for (let i = 0; i < this.points_.length - 1; i++) {
+			const p = this.points_[i];
+			const pPlusOne = this.points_[i + 1];
+			if (p.x <= x && (!previousPoint || p.x > previousPoint.x)) {
+				previousPoint = p;
+				nextPoint = pPlusOne;
+			}
+		}
+
+		if (!previousPoint || !nextPoint) {
+			throw new Error('Sampled point not on curve!');
+		}
+		const dx = nextPoint.x - previousPoint.x;
+		const dy = nextPoint.y - previousPoint.y;
+		const output = previousPoint.y + dy * ((x - previousPoint.x) / dx);
+		return output;
+	}
+}
 class MovingAverageFilter {
 	private windowSize: number;
 	private values: number[];
@@ -54,13 +89,15 @@ class LowPassFilter {
 	}
 }
 
-class MaxIncreaseFilter {
-	private maxRate: number;
+class MaxChangeFilter {
+	private maxIncrease: number | undefined;
+	private maxDecrease: number | undefined;
 	private previousOutput: number;
 	private desiredValue: number;
 
-	constructor(maxRate: number) {
-		this.maxRate = maxRate;
+	constructor(maxIncrease?: number, maxDecrease?: number) {
+		this.maxIncrease = maxIncrease;
+		this.maxDecrease = maxDecrease;
 		this.previousOutput = 0;
 		this.desiredValue = 0;
 	}
@@ -69,8 +106,13 @@ class MaxIncreaseFilter {
 		this.desiredValue = input;
 		const delta = (this.desiredValue - this.previousOutput) / dt;
 
-		if (delta > this.maxRate) {
-			const output = this.previousOutput + this.maxRate * dt;
+		if (this.maxIncrease !== undefined && delta > this.maxIncrease) {
+			const output = this.previousOutput + this.maxIncrease * dt;
+			this.previousOutput = output;
+			return output;
+		}
+		if (this.maxDecrease !== undefined && delta < this.maxDecrease) {
+			const output = this.previousOutput + this.maxDecrease * dt;
 			this.previousOutput = output;
 			return output;
 		}
@@ -161,22 +203,51 @@ function sameColor(x: Color, y: Color) {
 	return x.r === y.r && x.g === y.g && x.b === y.b;
 }
 
+// PID Tuning Parameters
+const steeringDampener = new MaxChangeFilter(12.5, -12.5);
 const MAX_STEERING_AMOUNT = 1;
-const THROTTLE_BASE = 0.095;
-const THROTTLE_INCREMENT = 0.13;
-const THROTTLE_INCREMENT_STEER_DROPOFF_RANGE = 0.3;
-const TOP_ESTIMATED_SPEED_LIMIT = 360;
-const KP = 0.85;
+const KP = 0.8;
 const KI = 0;
-const KD = 1 / 105;
+const KD = 1 / 90;
 const D_LOWPASS_TC = 1 / 2;
 const INTEGRATOR_MAX_FACTOR = 0.25;
+
+// Throttle tuning
+const steerTrackingLpf = new LowPassFilter(1 / 2);
+const throttleFilter = new MaxChangeFilter(0.55, undefined);
+const throttleCurve = new SamplingCurve([
+	{
+		x: 0,
+		y: 0.3,
+	},
+	{
+		x: 0.2,
+		y: 0.23,
+	},
+	{
+		x: 0.4,
+		y: 0.17,
+	},
+	{
+		x: 0.6,
+		y: 0.12,
+	},
+	{
+		x: 0.8,
+		y: 0.092,
+	},
+	{
+		x: 1,
+		y: 0.09,
+	},
+]);
+
+// Turn detection
 const TURN_DETECTION_STEER_THRESHOLD = 0.275;
 const TURN_DETECTION_TIME_TRESHOLD = 0.25;
 const TURN_HISTORY_LENGTH = 35;
-const avgThrottleTracker = new MovingAverageFilter(8);
 const avgSteerTracker = new MovingAverageFilter(6);
-const turnDetectionLpf = new LowPassFilter(0.4);
+const turnDetectionLpf = new LowPassFilter(1 / 6);
 
 const steeringPID = new PIDController(
 	KP,
@@ -190,7 +261,6 @@ const steeringPID = new PIDController(
 );
 
 let previousTimestamp = Date.now();
-
 let currentTurn = 'S';
 let detectedTurn = 'S';
 interface TurnHistoryEntry {
@@ -316,86 +386,31 @@ const calculateTurn = (frame: Frame) => {
 	return turn;
 };
 
+const calculateThrottle = (steering: number, dt: number) => {
+	const steeringFiltered = steerTrackingLpf.filter(steering);
+	const throttle = throttleCurve.getValue(Math.abs(steeringFiltered));
+	const filteredThrottle = throttleFilter.filter(throttle, dt);
+	return filteredThrottle;
+};
+
 function decide(frame: Frame) {
 	const timeStamp = Date.now();
 	const dt = (timeStamp - previousTimestamp) / 1000;
 	previousTimestamp = timeStamp;
-	const detectedTurn = turnDetectionLpf.filter(calculateTurn(frame));
 
+	const detectedTurn = turnDetectionLpf.filter(calculateTurn(frame));
 	predictedTurns = predictTurnsViaHistory();
 
-	let boost = false;
-	if (
-		predictedTurns &&
-		predictedTurns[0].turn === 'S' &&
-		timeSpentInCurrentTurn < predictedTurns[0].duration * 0.45 &&
-		predictedTurns[0].duration > 1.2
-	) {
-		boost = true;
-	}
-	if (
-		predictedTurns &&
-		predictedTurns[1].turn === 'S' &&
-		timeSpentInCurrentTurn > predictedTurns[0].duration * 0.35 &&
-		predictedTurns[0].duration + predictedTurns[1].duration > 2
-	) {
-		boost = true;
-	}
+	const steering = steeringPID.update(detectedTurn, dt);
+	const steeringDampened = steeringDampener.filter(steering, dt);
+	const throttle = calculateThrottle(steeringDampened, dt);
 
-	let offset = 0;
-	if (
-		predictedTurns &&
-		predictedTurns[0].turn === 'S' &&
-		predictedTurns[0].duration > 0.65 &&
-		timeSpentInCurrentTurn > 0.25 &&
-		predictedTurns[0].duration - timeSpentInCurrentTurn > 0.35
-	) {
-		if (predictedTurns[1].turn === 'R') {
-			offset = 0.1;
-		} else {
-			offset = -0.1;
-		}
-	}
-	if (
-		predictedTurns &&
-		predictedTurns[0].turn !== 'S' &&
-		predictedTurns[1].turn === 'S' &&
-		timeSpentInCurrentTurn > predictedTurns[0].duration * 0.25
-	) {
-		if (predictedTurns[0].turn === 'R') {
-			offset = 0.06;
-		} else {
-			offset = -0.06;
-		}
-	}
-	const steering = steeringPID.update(detectedTurn + offset, dt);
-	let throttle =
-		THROTTLE_BASE +
-		THROTTLE_INCREMENT *
-			(1 -
-				Math.min(
-					1,
-					Math.abs(steering) /
-						(THROTTLE_INCREMENT_STEER_DROPOFF_RANGE * MAX_STEERING_AMOUNT)
-				));
-
-	const estSpeed = avgThrottleTracker.getAverage() * 1010 + 64;
-
-	avgThrottleTracker.filter(throttle);
-	avgSteerTracker.filter(steering);
+	avgSteerTracker.filter(steeringDampened);
 	turningTracker(avgSteerTracker.getAverage(), dt);
 
-	if (boost) {
-		throttle = Math.max(throttle * 1.1, 0.165);
-	}
-	//if (throttle > throttleLimit && !boost) throttle = throttleLimit;
-
-	throttle = Math.min(0.275, Math.max(0, throttle));
 	return {
 		throttle,
-		boost,
-		estSpeed,
-		steering,
+		steering: steeringDampened,
 		currentTurn,
 		timeSpentInCurrentTurn,
 		turnHistory: turnHistory
@@ -406,7 +421,6 @@ function decide(frame: Frame) {
 			?.slice(-4)
 			.map((t) => `${t.duration.toFixed(1)} ${t.turn}`)
 			.join(', '),
-		offset,
 		pOut: steeringPID.pOut,
 		iOut: steeringPID.iOut,
 		dOut: steeringPID.dOut,
